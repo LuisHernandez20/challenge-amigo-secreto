712938211f49877de1f77d9dde36b46685727b64
LuisHernandez20 <al221610944@gmail.com>
2025-01-26 22:50:54 -0600

    Se agregan funciones de agregarElementoLista, limpiarElemento, agregarAmigo al archivo app.js

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function limpiarElemento(elemento) {
+    document.getElementById(elemento).value = '';
+}
+
+function agregarElementoLista(elemento, texto) {
+    var ul = document.getElementById(elemento);
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(texto));
+    //li.setAttribute("id", "element4"); // added line
+    ul.appendChild(li);
+    // alert(li.id);
+  }
+
+function agregarAmigo() {
+    let nombreDeAmigo = document.getElementById('amigo').value;
+     console.log(nombreDeAmigo);
+     
+    if (nombreDeAmigo == "" || nombreDeAmigo == null) {
+        alert(`Porfavor ingresa un nombre válido`);
+        limpiarElemento('amigo');
+
+    } else {
+        //El usuario ingreso la informacion correcta.
+     console.log(listaAmigos);
+     listaAmigos.push(nombreDeAmigo);
+
+     agregarElementoLista('listaAmigos',`${nombreDeAmigo}`);
+
+     limpiarElemento('amigo');
+
+       
+    }
+    return;
+}
