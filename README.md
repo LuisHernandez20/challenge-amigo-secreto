d49a74d8b87daac45a22e2e744b22ace42f4daae
LuisHernandez20 <al221610944@gmail.com>
2025-01-27 19:40:42 -0600

    Se agrega informacion de archivo readme, asi mismo se agregan comentarios y funciones al archivo apps.js

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # challenge-amigo-secreto
 
 Inicio de challenge amigo secreto en js con html y css
+
+Este es un entrenamiento de las habilidades adquiridas en los cursos: 
+- Lógica de programación: sumérgete en la programación con JavaScript(JS)
+- Lógica de programación: explorar funciones y listas
+- Git y GitHub: repositorio, commit y versiones
+
+Los cuales se muestran en este challenge con el porposito de realizar las funciones estructuradas con el fin de reducir el numero de procesos que pudieran ser repetitivos en JS, permitiendo un código simple y facil de entender no solo para nosotros si no para otros colaboradores que pudieran estar dentro del entorno de trabajo.
+
+El proyecto es de una sola pagina y puede ejecutarse en cualquier equipo sin necesitas de un emulador de servicios esto mediante el archivo index.html que viene en la carpeta asi mismo seleccionando el navegador de su preferencia como lo puede ser chrome, mozilla firefox, opera, brave, haciendo incapie en chrome ya que es el mas utilizado por los usuarios.
+
+El proyecto fue creado mediante el el editor de codigo visual studio code, con la extension de live server para reflejar los cambios realizados al codigo en tiempo real.
--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 var listaAmigos = [];
 
+// Esta función permite la limpieza de cualquier elemento selecionandolo por el id(elemento), pudiendo ser un <p> o un <input>, o cualquier elemento que pueda manejar información
 function limpiarElemento(elemento) {
     document.getElementById(elemento).value = '';
     document.getElementById(elemento).innerHTML = "";
-    
-    
 }
 
+// Función que permite agregar los nosmbres ingresados a una lista(esta puede cualquier lista que pudiera existir en el html ya que los parametros son variables que pueden ser diferentes, dependiendo de ocmo se invoque)
 function agregarElementoLista(elemento, texto) {
     var ul = document.getElementById(elemento);
     var li = document.createElement("li");
@@ -15,6 +15,7 @@ function agregarElementoLista(elemento, texto) {
     ul.appendChild(li);
   }
 
+  // Validación de texto ingresado, asi como el agregado del nombre a un array para despues utilizarse de la forma que mas convenga
 function agregarAmigo() {
     let nombreDeAmigo = document.getElementById('amigo').value;
      console.log(nombreDeAmigo);
@@ -44,10 +45,14 @@ function agregarAmigo() {
 }
 
 function sortearAmigo() {
+    // Función que nos permite obtener un numero aleatorio mediante la cantidad de nombre ingresado por el usuario
     let indiceAleatorio =  Math.floor(Math.random()*listaAmigos.length);
     console.log(indiceAleatorio);
     amigoSecreto = listaAmigos[indiceAleatorio];
     limpiarElemento('listaAmigos');
     agregarElementoLista('resultado',`El amigo secreto es: ${amigoSecreto}`);
-   
+    alert(
+        "El sorteo se ha realizado. Para reiniciar, recarga la pagina'."
+      );
+      document.getElementById("botonSorteo").disabled = true;
 }
